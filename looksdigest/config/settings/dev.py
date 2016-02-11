96b6c319a7112d4745d84a9ab559ff5928bbe4e0
Andy Idiaghe <andy.idiaghe@gmail.com>
2016-02-11 18:10:04 +0100

    ENH: Modifid settings to grab variables from secrets

--- looksdigest/config/settings/dev.py
+++ looksdigest/config/settings/dev.py
@@ -2,3 +2,17 @@ from looksdigest.config.settings.base import *
 
 DJANGO_SETTINGS_MODULE = 'looksdigest.config.settings.dev'
 ENV = 'dev'
+loader = LoadConf(ENV)
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': loader.get_secret('DJANGO_DB_ENGINE'),
+        'NAME': loader.get_secret('DJANGO_DB_NAME'),
+        'USER': loader.get_secret('DJANGO_DB_USER'),
+        'PASSWORD': loader.get_secret('DJANGO_DB_PASSWORD'),
+        'HOST': loader.get_secret('DJANGO_DB_HOST'),
+        'PORT': loader.get_secret('DJANGO_DB_PORT')
+    }
+}

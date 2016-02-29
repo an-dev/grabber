cd48a59d3afcb672e8c5bc75f2f5a4e3c1f9bcde
Andy Idiaghe <andy.idiaghe@gmail.com>
2016-02-29 10:17:16 +0100

    ENH: Modified fabfile with migrate, make_migration and shell cmds

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,12 @@
 import sys
 import os
+import logging
+
+from distutils.util import strtobool
+
 from fabric.api import *
 from fabric.contrib import django
-from distutils.util import strtobool
-import logging
+
 
 logger = logging.getLogger(__name__)
 
@@ -26,19 +29,20 @@ def set_fab_env(function):
     """
         Custom decorator to import specific env
     """
+    @task(alias=function.__name__)
     def wrap_function(*args, **kwargs):
-        env = DEFAULT_ENV
-
         if len(args) > 0 and 'env' in kwargs:
             raise TypeError('%s() got multiple values for keyword argument "env"' % function.__name__)
 
         if len(args) > 0:
-            env = args[0]
+            environ = args[0]
         else:
             if 'env' in kwargs:
-                env = kwargs['env']
+                environ = kwargs['env']
+            else:
+                environ = DEFAULT_ENV
 
-        django.settings_module('{}.config.settings.{}'.format(DJANGO_PROJECT, env))
+        django.settings_module('{}.config.settings.{}'.format(DJANGO_PROJECT, environ))
 
         try:
             logger.debug('args = %s' % str(args))
@@ -46,15 +50,16 @@ def set_fab_env(function):
             return function(*args, **kwargs)
         except Exception as e:
             raise e
+
+    wrap_function.__doc__ = function.__doc__
+
     return wrap_function
 
 
 @set_fab_env
-def runserver(env=DEFAULT_ENV, autoreload=True):
+def runserver(environ=DEFAULT_ENV, autoreload=True):
     """
-        Runserver task
-        @env: environment in which to run the server
-        @autoreload: wheather to use or not --noreload flag
+        Runserver task, takes in env, autoreload(bool)
     """
     # TODO: get address:port from CLI
     if booleanize(autoreload):
@@ -65,9 +70,24 @@ def runserver(env=DEFAULT_ENV, autoreload=True):
 
 
 @set_fab_env
-def migrate():
+def shell(environ=DEFAULT_ENV):
+    """
+        IPython Shell task
+    """
+    local('python manage.py shell -i ipython')
+
+
+@set_fab_env
+def migrate(environ=DEFAULT_ENV):
     """
         Migrate task
     """
-    # TODO: migrate per app
-    local('python manage.py migrate')
+    local('python manage.py migrate --run-syncdb')
+
+
+@set_fab_env
+def make_migrations(environ=DEFAULT_ENV):
+    """
+        Create Migration task
+    """
+    local('python manage.py makemigrations')
